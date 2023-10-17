91fb9392fc455ff5f0250625ee6d460af5b7f22b
twinkle-goyal <twinkle.goyal@hp.com>
2023-10-17 12:06:20 +0530

    initial commit

---
+++ PFV_Response_Object_Structure2.js
@@ -0,0 +1,452 @@
+const Response = {
+  productId: 311,
+  productName: "OfficeJet Pro 8022e",
+  competitorProducts: [
+    {
+      competitorProductId: 1,
+      competitorProductName: "WorkForce Pro WF-3820DWF",
+      priceSource: [
+        {
+          priceSourceId: 1,
+          priceSourceName: "WorkIT DE CYBERPORT",
+          weeklyData: [
+            {
+              date: "01-01-2023",
+              hpAspLocal: 183,
+              compAspLocal: 147,
+              actualPfv: 1.2,
+            },
+            {
+              date: "08-01-2023",
+              hpAspLocal: 184,
+              compAspLocal: 148,
+              actualPfv: 1.1,
+            },
+            {
+              date: "15-01-2023",
+              hpAspLocal: 185,
+              compAspLocal: 149,
+              actualPfv: 1.6,
+            },
+            {
+              date: "22-01-2023",
+              hpAspLocal: 186,
+              compAspLocal: 150,
+              actualPfv: 1.4,
+            },
+            {
+              date: "29-01-2023",
+              hpAspLocal: 187,
+              compAspLocal: 151,
+              actualPfv: 1.3,
+            },
+          ],
+        },
+        {
+          priceSourceId: 2,
+          priceSourceName: "WorkIT DE EXPERT",
+          weeklyData: [
+            {
+              date: "01-01-2023",
+              hpAspLocal: 183,
+              compAspLocal: 147,
+              actualPfv: 1.2,
+            },
+            {
+              date: "08-01-2023",
+              hpAspLocal: 184,
+              compAspLocal: 148,
+              actualPfv: 1.1,
+            },
+            {
+              date: "15-01-2023",
+              hpAspLocal: 185,
+              compAspLocal: 149,
+              actualPfv: 1.6,
+            },
+            {
+              date: "22-01-2023",
+              hpAspLocal: 186,
+              compAspLocal: 150,
+              actualPfv: 1.4,
+            },
+            {
+              date: "29-01-2023",
+              hpAspLocal: 187,
+              compAspLocal: 151,
+              actualPfv: 1.3,
+            },
+          ],
+        },
+        {
+          priceSourceId: 3,
+          priceSourceName: "WorkIT DE MEDIAMARKT",
+          weeklyData: [
+            {
+              date: "01-01-2023",
+              hpAspLocal: 183,
+              compAspLocal: 147,
+              actualPfv: 1.2,
+            },
+            {
+              date: "08-01-2023",
+              hpAspLocal: 184,
+              compAspLocal: 148,
+              actualPfv: 1.1,
+            },
+            {
+              date: "15-01-2023",
+              hpAspLocal: 185,
+              compAspLocal: 149,
+              actualPfv: 1.6,
+            },
+            {
+              date: "22-01-2023",
+              hpAspLocal: 186,
+              compAspLocal: 150,
+              actualPfv: 1.4,
+            },
+            {
+              date: "29-01-2023",
+              hpAspLocal: 187,
+              compAspLocal: 151,
+              actualPfv: 1.3,
+            },
+          ],
+        },
+        {
+          priceSourceId: 4,
+          priceSourceName: "WorkIT DE OTTO",
+          weeklyData: [
+            {
+              date: "01-01-2023",
+              hpAspLocal: 183,
+              compAspLocal: 147,
+              actualPfv: 1.2,
+            },
+            {
+              date: "08-01-2023",
+              hpAspLocal: 184,
+              compAspLocal: 148,
+              actualPfv: 1.1,
+            },
+            {
+              date: "15-01-2023",
+              hpAspLocal: 185,
+              compAspLocal: 149,
+              actualPfv: 1.6,
+            },
+            {
+              date: "22-01-2023",
+              hpAspLocal: 186,
+              compAspLocal: 150,
+              actualPfv: 1.4,
+            },
+            {
+              date: "29-01-2023",
+              hpAspLocal: 187,
+              compAspLocal: 151,
+              actualPfv: 1.3,
+            },
+          ],
+        },
+        {
+          priceSourceId: 5,
+          priceSourceName: "WorkIT DE SATURN",
+          weeklyData: [
+            {
+              date: "01-01-2023",
+              hpAspLocal: 183,
+              compAspLocal: 147,
+              actualPfv: 1.2,
+            },
+            {
+              date: "08-01-2023",
+              hpAspLocal: 184,
+              compAspLocal: 148,
+              actualPfv: 1.1,
+            },
+            {
+              date: "15-01-2023",
+              hpAspLocal: 185,
+              compAspLocal: 149,
+              actualPfv: 1.6,
+            },
+            {
+              date: "22-01-2023",
+              hpAspLocal: 186,
+              compAspLocal: 150,
+              actualPfv: 1.4,
+            },
+            {
+              date: "29-01-2023",
+              hpAspLocal: 187,
+              compAspLocal: 151,
+              actualPfv: 1.3,
+            },
+          ],
+        },
+        {
+          priceSourceId: 6,
+          priceSourceName: "WorkIT DE AMAZON",
+          weeklyData: [
+            {
+              date: "01-01-2023",
+              hpAspLocal: 183,
+              compAspLocal: 147,
+              actualPfv: 1.2,
+            },
+            {
+              date: "08-01-2023",
+              hpAspLocal: 184,
+              compAspLocal: 148,
+              actualPfv: 1.1,
+            },
+            {
+              date: "15-01-2023",
+              hpAspLocal: 185,
+              compAspLocal: 149,
+              actualPfv: 1.6,
+            },
+            {
+              date: "22-01-2023",
+              hpAspLocal: 186,
+              compAspLocal: 150,
+              actualPfv: 1.4,
+            },
+            {
+              date: "29-01-2023",
+              hpAspLocal: 187,
+              compAspLocal: 151,
+              actualPfv: 1.3,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      competitorProductId: 2,
+      competitorProductName: "MFC-J4335DW",
+      priceSource: [
+        {
+          priceSourceId: 7,
+          priceSourceName: "WorkIT DE CYBERPORT",
+          weeklyData: [
+            {
+              date: "01-01-2023",
+              hpAspLocal: 183,
+              compAspLocal: 147,
+              actualPfv: 1.2,
+            },
+            {
+              date: "08-01-2023",
+              hpAspLocal: 184,
+              compAspLocal: 148,
+              actualPfv: 1.1,
+            },
+            {
+              date: "15-01-2023",
+              hpAspLocal: 185,
+              compAspLocal: 149,
+              actualPfv: 1.6,
+            },
+            {
+              date: "22-01-2023",
+              hpAspLocal: 186,
+              compAspLocal: 150,
+              actualPfv: 1.4,
+            },
+            {
+              date: "29-01-2023",
+              hpAspLocal: 187,
+              compAspLocal: 151,
+              actualPfv: 1.3,
+            },
+          ],
+        },
+        {
+          priceSourceId: 8,
+          priceSourceName: "WorkIT DE EXPERT",
+          weeklyData: [
+            {
+              date: "01-01-2023",
+              hpAspLocal: 183,
+              compAspLocal: 147,
+              actualPfv: 1.2,
+            },
+            {
+              date: "08-01-2023",
+              hpAspLocal: 184,
+              compAspLocal: 148,
+              actualPfv: 1.1,
+            },
+            {
+              date: "15-01-2023",
+              hpAspLocal: 185,
+              compAspLocal: 149,
+              actualPfv: 1.6,
+            },
+            {
+              date: "22-01-2023",
+              hpAspLocal: 186,
+              compAspLocal: 150,
+              actualPfv: 1.4,
+            },
+            {
+              date: "29-01-2023",
+              hpAspLocal: 187,
+              compAspLocal: 151,
+              actualPfv: 1.3,
+            },
+          ],
+        },
+        {
+          priceSourceId: 9,
+          priceSourceName: "WorkIT DE MEDIAMARKT",
+          weeklyData: [
+            {
+              date: "01-01-2023",
+              hpAspLocal: 183,
+              compAspLocal: 147,
+              actualPfv: 1.2,
+            },
+            {
+              date: "08-01-2023",
+              hpAspLocal: 184,
+              compAspLocal: 148,
+              actualPfv: 1.1,
+            },
+            {
+              date: "15-01-2023",
+              hpAspLocal: 185,
+              compAspLocal: 149,
+              actualPfv: 1.6,
+            },
+            {
+              date: "22-01-2023",
+              hpAspLocal: 186,
+              compAspLocal: 150,
+              actualPfv: 1.4,
+            },
+            {
+              date: "29-01-2023",
+              hpAspLocal: 187,
+              compAspLocal: 151,
+              actualPfv: 1.3,
+            },
+          ],
+        },
+        {
+          priceSourceId: 10,
+          priceSourceName: "WorkIT DE OTTO",
+          weeklyData: [
+            {
+              date: "01-01-2023",
+              hpAspLocal: 183,
+              compAspLocal: 147,
+              actualPfv: 1.2,
+            },
+            {
+              date: "08-01-2023",
+              hpAspLocal: 184,
+              compAspLocal: 148,
+              actualPfv: 1.1,
+            },
+            {
+              date: "15-01-2023",
+              hpAspLocal: 185,
+              compAspLocal: 149,
+              actualPfv: 1.6,
+            },
+            {
+              date: "22-01-2023",
+              hpAspLocal: 186,
+              compAspLocal: 150,
+              actualPfv: 1.4,
+            },
+            {
+              date: "29-01-2023",
+              hpAspLocal: 187,
+              compAspLocal: 151,
+              actualPfv: 1.3,
+            },
+          ],
+        },
+        {
+          priceSourceId: 11,
+          priceSourceName: "WorkIT DE SATURN",
+          weeklyData: [
+            {
+              date: "01-01-2023",
+              hpAspLocal: 183,
+              compAspLocal: 147,
+              actualPfv: 1.2,
+            },
+            {
+              date: "08-01-2023",
+              hpAspLocal: 184,
+              compAspLocal: 148,
+              actualPfv: 1.1,
+            },
+            {
+              date: "15-01-2023",
+              hpAspLocal: 185,
+              compAspLocal: 149,
+              actualPfv: 1.6,
+            },
+            {
+              date: "22-01-2023",
+              hpAspLocal: 186,
+              compAspLocal: 150,
+              actualPfv: 1.4,
+            },
+            {
+              date: "29-01-2023",
+              hpAspLocal: 187,
+              compAspLocal: 151,
+              actualPfv: 1.3,
+            },
+          ],
+        },
+        {
+          priceSourceId: 12,
+          priceSourceName: "WorkIT DE AMAZON",
+          weeklyData: [
+            {
+              date: "01-01-2023",
+              hpAspLocal: 183,
+              compAspLocal: 147,
+              actualPfv: 1.2,
+            },
+            {
+              date: "08-01-2023",
+              hpAspLocal: 184,
+              compAspLocal: 148,
+              actualPfv: 1.1,
+            },
+            {
+              date: "15-01-2023",
+              hpAspLocal: 185,
+              compAspLocal: 149,
+              actualPfv: 1.6,
+            },
+            {
+              date: "22-01-2023",
+              hpAspLocal: 186,
+              compAspLocal: 150,
+              actualPfv: 1.4,
+            },
+            {
+              date: "29-01-2023",
+              hpAspLocal: 187,
+              compAspLocal: 151,
+              actualPfv: 1.3,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+module.exports = Response;
---
+++ index.js
@@ -0,0 +1,15 @@
+const Response  = require("./PFV_Response_Object_Structure2");
+
+const express = require("express");
+
+const app = express();
+
+const PORT = 8001;
+
+app.get(`/api/pfv-account/:countryId/:SegmentId/:productId`, (req, res) => {
+  return res.status(200).send(Response);
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
